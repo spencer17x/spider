8defd63d2f6e45daeec2756d85b86ec16fa7ed36
Spencer17x <1253478653@qq.com>
2022-04-16 20:08:36 +0800

    refactor: code optimize

--- task/cookie.go
+++ task/cookie.go
@@ -11,63 +11,56 @@ import (
 	"strings"
 )
 
-// SaveCookie save cookie
-func SaveCookie() chromedp.ActionFunc {
-	return func(ctx context.Context) (err error) {
-		log.Println("save cookie...")
-		if err = chromedp.WaitReady(`body`, chromedp.ByQuery).Do(ctx); err != nil {
-			return
-		}
-
-		cookies, err := network.GetAllCookies().Do(ctx)
-		if err != nil {
-			return err
-		}
-
-		cookiesData, err := network.GetAllCookiesReturns{Cookies: cookies}.MarshalJSON()
-		if err != nil {
-			return err
-		}
-
-		if err = ioutil.WriteFile(config.CookieFile, cookiesData, 0755); err != nil {
-			return err
-		}
-		return nil
+func saveCookie(ctx context.Context) error {
+	log.Println("saveCookie...")
+	if err := chromedp.WaitReady(`body`, chromedp.ByQuery).Do(ctx); err != nil {
+		return err
 	}
+
+	cookies, err := network.GetAllCookies().Do(ctx)
+	if err != nil {
+		return err
+	}
+
+	cookiesData, err := network.GetAllCookiesReturns{Cookies: cookies}.MarshalJSON()
+	if err != nil {
+		return err
+	}
+
+	if err := ioutil.WriteFile(config.CookieFile, cookiesData, 0755); err != nil {
+		return err
+	}
+	return nil
 }
 
-// CheckLoginStatus check login status
-func CheckLoginStatus() chromedp.ActionFunc {
-	return func(ctx context.Context) (err error) {
-		var url string
-		if err = chromedp.Evaluate(`window.location.href`, &url).Do(ctx); err != nil {
-			return
-		}
-		if strings.Contains(url, "https://account.wps.cn/usercenter/apps") {
-			log.Println("已经使用cookies登陆")
-			chromedp.Stop()
-		}
+// checkLoginStatus check login status
+func checkLoginStatus(ctx context.Context) (err error) {
+	var url string
+	if err = chromedp.Evaluate(`window.location.href`, &url).Do(ctx); err != nil {
 		return
 	}
+	if strings.Contains(url, "https://account.wps.cn/usercenter/apps") {
+		log.Println("已经使用cookies登陆")
+		chromedp.Stop()
+	}
+	return
 }
 
-// LoadCookie load cookie
-func LoadCookie() chromedp.ActionFunc {
-	return func(ctx context.Context) (err error) {
-		if _, _err := os.Stat("cookies.tmp"); os.IsNotExist(_err) {
-			return
-		}
-
-		cookiesData, err := ioutil.ReadFile("cookies.tmp")
-		if err != nil {
-			return
-		}
-
-		cookiesParams := network.SetCookiesParams{}
-		if err = cookiesParams.UnmarshalJSON(cookiesData); err != nil {
-			return
-		}
-
-		return network.SetCookies(cookiesParams.Cookies).Do(ctx)
+// loadCookie load cookie
+func loadCookie(ctx context.Context) (err error) {
+	if _, _err := os.Stat("cookies.tmp"); os.IsNotExist(_err) {
+		return
 	}
+
+	cookiesData, err := ioutil.ReadFile("cookies.tmp")
+	if err != nil {
+		return
+	}
+
+	cookiesParams := network.SetCookiesParams{}
+	if err = cookiesParams.UnmarshalJSON(cookiesData); err != nil {
+		return
+	}
+
+	return network.SetCookies(cookiesParams.Cookies).Do(ctx)
 }
--- task/login.go
+++ task/login.go
@@ -10,47 +10,36 @@ import (
 	"time"
 )
 
-// CreateTasks task to run
-func CreateTasks() chromedp.Tasks {
-	return chromedp.Tasks{
-		chromedp.Navigate(
-			config.URL,
-		),
-		chromedp.Click(`.login-button`, chromedp.ByQuery),
-		chromedp.Click(`.prompt-box .clickable`, chromedp.ByQuery),
-		chromedp.Click(`.oauth-bg .oauth-btn[title="微信"]`, chromedp.ByQuery),
-		WeChatLoginTask(),
-		chromedp.WaitVisible(
-			`#juejin > div.view-container.container > div > header > div > nav > ul > ul > li.nav-item.menu > div > img`,
-			chromedp.ByQuery,
-		),
-		SaveCookie(),
-	}
-}
+type Task func(context.Context) error
 
-// WeChatLoginTask new tab to weChat login
-func WeChatLoginTask() chromedp.ActionFunc {
+// createTask task for chromedp
+func createTask(task Task) chromedp.ActionFunc {
 	return func(ctx context.Context) error {
-		log.Println("weChat login...")
-		ch := chromedp.WaitNewTarget(ctx, func(info *target.Info) bool {
-			return info.URL != ""
-		})
-		newCtx, _ := chromedp.NewContext(ctx, chromedp.WithTargetID(<-ch))
-
-		var urlStr string
-		if err := chromedp.Run(newCtx, chromedp.Tasks{
-			chromedp.Location(&urlStr),
-			utils.GetQRCode(),
-		}); err != nil {
-			log.Fatal(err)
-		}
-		return nil
+		return task(ctx)
 	}
 }
 
-// Login entry
-func Login() {
-	log.Println("login...")
+// newTargetToShowQrCode new tab to WeChat login
+func newTargetToShowQrCode(ctx context.Context) error {
+	log.Println("showQrCode to logging...")
+	ch := chromedp.WaitNewTarget(ctx, func(info *target.Info) bool {
+		return info.URL != ""
+	})
+	newCtx, _ := chromedp.NewContext(ctx, chromedp.WithTargetID(<-ch))
+
+	var urlStr string
+	if err := chromedp.Run(newCtx, chromedp.Tasks{
+		chromedp.Location(&urlStr),
+		utils.GetQRCode(),
+	}); err != nil {
+		log.Fatal(err)
+	}
+	return nil
+}
+
+// WeChatLogin login entry
+func WeChatLogin() {
+	log.Println("WeChat logging...")
 	ctx, _ := chromedp.NewExecAllocator(
 		context.Background(),
 		append(
@@ -60,12 +49,24 @@ func Login() {
 	)
 
 	ctx, _ = context.WithTimeout(ctx, 1*time.Minute)
+
 	ctx, _ = chromedp.NewContext(
 		ctx,
 		chromedp.WithLogf(log.Printf),
 	)
 
-	if err := chromedp.Run(ctx, CreateTasks()); err != nil {
+	if err := chromedp.Run(ctx, chromedp.Tasks{
+		chromedp.Navigate(config.URL),
+		chromedp.Click(`.login-button`, chromedp.ByQuery),
+		chromedp.Click(`.prompt-box .clickable`, chromedp.ByQuery),
+		chromedp.Click(`.oauth-bg .oauth-btn[title="微信"]`, chromedp.ByQuery),
+		createTask(newTargetToShowQrCode),
+		chromedp.WaitVisible(
+			`#juejin > div.view-container.container > div > header > div > nav > ul > ul > li.nav-item.menu > div > img`,
+			chromedp.ByQuery,
+		),
+		createTask(saveCookie),
+	}); err != nil {
 		log.Fatal(err)
 	}
 }
